d3a6d1215e1d31c559050f1b3161bfcaf5e5e2c9
Haru Ichi <jarunee.recc2@gmail.com>
2025-09-11 12:27:42 +0700

    remove new badge navbar

--- template/navbar.php
+++ template/navbar.php
@@ -91,11 +91,11 @@ function navbar($report_count = null)
                     </div>
                     <div class="dropdown">
                         <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false" style="color: #fff; margin-top: 1px">
-                            งาน <span class="badge bg-danger">new</span>
+                            งาน
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
                             <li><a class="dropdown-item" href="myjob.php">งานของฉัน</a></li>
-                            <li><a class="dropdown-item" href="routineJob.php">จัดการงาน Daily <span class="badge bg-danger">new</span></a></li>
+                            <li><a class="dropdown-item" href="routineJob.php">จัดการงาน Daily</a></li>
                         </ul>
                     </div>
                     <li class="nav-item">
